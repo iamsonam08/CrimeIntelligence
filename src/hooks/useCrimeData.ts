4ffbb543b3a6dd44d24e77a8bd951fb6b8d4c214
himanshu-29-tech <himanshuaman92@gmail.com>
2026-07-25 23:55:50 +0530

    Point frontend to deployed backend URL

--- src/hooks/useCrimeData.ts
+++ src/hooks/useCrimeData.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const API_BASE = 'http://127.0.0.1:8000';
+const API_BASE = 'https://crime-analytics-api.onrender.com';
 
 export function useCrimeData() {
   const [stats, setStats] = useState<any>(null);
